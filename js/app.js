628b090c8f31aef36c855b0c1cb23e033902b15b
Cornelius Kelechi Eze <ckelechieze@gmail.com>
2026-07-07 08:20:18 +0100

    add delete button

--- js/app.js
+++ js/app.js
@@ -14,6 +14,17 @@ const transactionList = document.getElementById('transactions-list');
 
 transactionForm.addEventListener('submit', function (event) {
     event.preventDefault();
+
+    if (descriptionInput.value === "" || amountInput.value === "" || dateInput.value === "") {
+      alert ("Please fill in all fields.");
+      return;
+    }
+
+    if (Number(amountInput.value) <= 0) {
+      alert("Amount must be greater than zero.");
+      return;
+    }
+
   const transaction = {
     description: descriptionInput.value,
     amount: Number(amountInput.value),
@@ -22,20 +33,24 @@ transactionForm.addEventListener('submit', function (event) {
   };
 
   transactions.push(transaction);
-  console.log(transactions);
   displayTransactions();
   updateSummary();
+  transactionForm.reset();
 });
 
 // Function to display transactions in the list
 function displayTransactions() {
   transactionList.innerHTML = "";
-  for (const transaction of transactions) {
+  for (let i = 0; i < transactions.length; i++) {
+    const transaction = transactions[i];
     const transactionCard = document.createElement('div');
     transactionCard.className = "flex justify-between items-center bg-gray-200 rounded-lg p-4 mb-3";
 
     const leftSection = document.createElement('div');
     leftSection.className = "flex flex-col";
+
+    const rightSection = document.createElement('div');
+    rightSection.className = "flex flex-col items-end";
 
     const description = document.createElement('p');
     description.textContent = transaction.description;
@@ -45,6 +60,18 @@ function displayTransactions() {
     date.classList.add("text-gray-500", "text-sm");
 
     const amount = document.createElement('p');
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = "Delete";
+    deleteButton.className = "ml-4 text-red-600 font-medium text-sm hover:text-red-800 bg-gray-200 px-2 py-1 rounded-lg";
+
+    // Add event listener to delete the transaction
+    deleteButton.addEventListener("click", function () {
+        transactions.splice(i, 1);
+        displayTransactions();
+        updateSummary();
+    })
+
     if (transaction.type === 'income') {
       amount.textContent = `+#${transaction.amount.toLocaleString()}`;
       amount.classList.add("text-green-600", "font-bold");
@@ -56,8 +83,11 @@ function displayTransactions() {
     leftSection.appendChild(description);
     leftSection.appendChild(date);
 
+     rightSection.appendChild(amount);
+     rightSection.appendChild(deleteButton);
+
     transactionCard.appendChild(leftSection);
-    transactionCard.appendChild(amount);
+    transactionCard.appendChild(rightSection);
     transactionList.appendChild(transactionCard);
   }
 }
